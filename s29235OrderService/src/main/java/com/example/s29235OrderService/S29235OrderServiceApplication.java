fe3f915983caed82bf8ddaa7832da5ade96c4dc3
Michal Fritza <fritzam96@gmail.com>
2024-01-31 15:55:26 +0100

    Initial commit

---
+++ s29235OrderService/src/main/java/com/example/s29235OrderService/S29235OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.s29235OrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29235OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S29235OrderServiceApplication.class, args);
+	}
+
+}
---
+++ s29235OrderService/src/test/java/com/example/s29235OrderService/S29235OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s29235OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S29235OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
